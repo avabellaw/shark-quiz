850bf08eb0df341027e2a28fb2622edecc38bdf1
avabellaw <70448139+avabellaw@users.noreply.github.com>
2023-11-13 22:43:06 +0000

    Add first question and display the values

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 let currentGameArea = "#home";
 
+let quiz = {
+    questions: [],
+    questionIndex: 0,
+    getQuestion: function() {
+        return questions[this.questionIndex];
+    },
+    nextQuestion: function() {
+        return questions[++this.questionIndex];
+    }
+};
+
 // Equivalent to $(document).ready( handler ) https://api.jquery.com/ready/
 $(init);
 
@@ -13,12 +24,31 @@ $("#start-quiz").on("click", () => {
  */
 function init(){ 
     swapGameArea("#quiz");
+    quiz.questions = questions;
 }
 
+/**
+ * Swaps which screen is displayed eg Home screen -> Quiz screen
+*/
 function swapGameArea(gameArea){
     $(currentGameArea).attr("data-visible", "false")
     currentGameArea = gameArea;
     $(currentGameArea).attr("data-visible", "true");
+
+    if(gameArea === "#quiz"){
+        showQuestions();
+    }
+}
+
+function showQuestions(){
+    let questionSet = quiz.getQuestion();
+    $("#question-box > h2").html(questionSet.question);
+    let answerBoxes = $(".answer-box");
+    
+    for(let i = 0; i < 3; i++) {
+        // Using [element].childNodes [https://www.w3schools.com/jsref/prop_node_childnodes.asp]
+        answerBoxes[i].childNodes[1].innerText = questionSet.options[i];
+    }
 }
 
 function getCurrentGameArea(){
---
+++ assets/js/questions.js
@@ -0,0 +1,7 @@
+const questions = [
+    {
+        question:"Do sharks sleep?",
+        options: ["True", "False", "Not often"],
+        answer: 0
+    }
+];
