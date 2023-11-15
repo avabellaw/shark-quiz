9d0fb85e5d3f6b2b210631ef656327a09c21860a
avabellaw <70448139+avabellaw@users.noreply.github.com>
2023-11-15 16:44:28 +0000

    feat: When timer==0, display correct/incorrect

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,8 +63,21 @@ function init() {
     randomiseAnswerPositions();
 }
 
-async function timerTick(timer) {
+function timerTick(timer) {
     $("#timer").val(timer);
+
+    if (timer === 0) {
+        quiz.answerQuestion(-1);
+        
+        // .each() wasn't working therfore I found a different way [https://stackoverflow.com/questions/4735342/jquery-to-loop-through-elements-with-the-same-class]
+        $.each($(".answer-box"), function (i, box) {
+            // This will loop through every .answer-box and apply the appropriate class 
+            if (i === quiz.getQuestion().answer)
+                $(box).addClass("correct-answer");
+            else
+                $(box).addClass("incorrect-answer");
+        });
+    }
 }
 
 function showCorrectAnswer() {
