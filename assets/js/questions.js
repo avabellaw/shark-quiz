bfab670523319de45eae8da04d9c5511bc56e605
avabellaw <70448139+avabellaw@users.noreply.github.com>
2023-11-16 01:04:54 +0000

    Add descriptions to all the questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,67 +9,83 @@ const questions = [
     },
     {
         question: "How many bones do sharks have?",
-        options: ["None", "206", "Over 500"]
+        options: ["None", "206", "Over 500"],
+        description: "A shark's skeleton is made of cartilage."
     },
     {
         question: "When do sharks learn how to attack?",
-        options: ["While they're embryos", "After 1 year", "When they hatch"]
+        options: ["While they're embryos", "After 1 year", "When they hatch"],
+        description: "They're known to cannibalize their siblings!"
     },
     {
         question: "What is a group of sharks called?",
-        options: ["Shoal", "Cluster", "Bale"]
+        options: ["Shoal", "Cluster", "Bale"],
+        description: "They're also called a school or a shiver."
     },
     {
         question: "What is a baby shark called?",
-        options: ["A pup", "A nestling", "A kit"]
+        options: ["A pup", "A nestling", "A kit"],
+        description: "Sharing its name with canines and seals."
     },
     {
         question: "In most Species, female sharks are bigger.",
-        options: ["True", "False"]
+        options: ["True", "False"],
+        description: "This helps them carry their pups (baby sharks)."
     },
     {
         question: "How many species of shark are there?",
-        options: ["500+", "Up to 300"]
+        options: ["500+", "Between 200-300"],
+        description: "Present in every ocean of the world!"
     },
     {
         question: "How does shark skin feel?",
-        options: ["Rough", "Slimey", "Soft"]
+        options: ["Rough", "Slimey", "Soft"],
+        description: "People describe it as feeling like sandpaper."
     }, 
     {
         question: "Do sharks have eyelids?",
-        options: ["Yes", "No"]
+        options: ["Yes", "No"],
+        description: "They don't need to blink but they're useful for protection."
     },
     {
         question: "How long can a Great White go without eating?",
-        options: ["1 month", "1 week", "2 months"]
+        options: ["1-2 months", "1 week", "2+ months"],
+        description: "Although it's highly unlikely they won't eat for over a month."
     },
     {
         question: "How many teeth can sharks have?",
-        options: ["Up to 3000", "Up to 100", "up to 1000"]
+        options: ["Up to 3,000", "Up to 100", "up to 1,000"],
+        description: "In about 5-15 rows!"
     },
     {
         question: "Can sharks swim backwards?",
-        options: ["No", "Yes"]
+        options: ["No", "Yes"],
+        description: "They can't stop suddenly like other fish either."
     },
     {
         question: "How many teeth will a shark lose in its lifetime?",
-        options: ["Around 30,000", "Up to 1,000", "Around 10,000"]
+        options: ["Around 30,000", "Up to 1,000", "Around 10,000"],
+        description: "They're constantly regrowing and lose a tooth about once a week."
     },
     {
         question: "The Whale Shark is the largest species.",
-        options: ["True", "False"]
+        options: ["True", "False"],
+        description: "The megalodon shark was bigger but is extinct. They were estimated to have been between 15-18 metres long."
     },
     {
         question: "How do sharks communicate?",
-        options: ["Body language", "Vibrations", "A clicking noise"]
+        options: ["Body language", "Vibrations", "A clicking noise"],
+        desciption: "They angle their body, fluff their gills, nod their heads and so on."
     },
     {
         question: "How is a shark's eyesight compared to a human's?",
-        options: ["Better", "Worse", "Similar"]
+        options: ["Better", "Worse", "Similar"],
+        desciption: "It's estimated to be about 10 times better!"
     },
     {
         question: "How do sharks like to hunt?",
-        options: ["Alone", "In large groups", "With 1-2 others"]
+        options: ["Alone", "In large groups", "With 1-2 others"],
+        desciption: "They primarly rely on the element of surprise. Some sharks will join up in certain circumstances."
     }
 ];
 
